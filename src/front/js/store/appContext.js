fcb8183f0f7dd42084447b66f5ea1d2d28e1869c
enaguero <enaguero@gmail.com>
2021-01-25 18:51:18 +0000

    First version without testing of entire flow to get list of planets

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -28,7 +28,11 @@ const injectContext = PassedComponent => {
 			 * you should do your ajax requests or fetch api requests here. Do not use setState() to save data in the
 			 * store, instead use actions, like this:
 			 **/
-			state.actions.getMessage(); // <---- calling this function from the flux.js actions
+      debugger
+      console.log(process.env.START_WARS_API);
+      
+      state.actions.getMessage(); // <---- calling this function from the flux.js actions
+      state.actions.getPlanets(process.env.START_WARS_API + "/people");
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
